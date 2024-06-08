875cf3ae1ac5a525bfdd4b401f9d91b87ce4f08d
Hanna <hanna.melnk@gmail.com>
2024-06-08 13:17:24 +0200

    prep exercise API week 1: done

--- Week1/prep-exercises/1-catwalk-promises/index.js
+++ Week1/prep-exercises/1-catwalk-promises/index.js
@@ -1,3 +1,4 @@
+//Week1/prep-exercises/1-catwalk-promises/index.js
 'use strict';
 
 const STEP_SIZE_PX = 10;
@@ -12,6 +13,20 @@ function walk(img, startPos, stopPos) {
     // `stopPos`.
     // Make good use of the `STEP_INTERVAL_PX` and `STEP_INTERVAL_MS`
     // constants.
+    let currentPos = startPos;
+    img.style.left = `${currentPos}px`;
+
+    function moveCat() {
+      if (currentPos < stopPos) {
+        currentPos += STEP_SIZE_PX;
+        img.style.left = `${currentPos}px`;
+        setTimeout(moveCat, STEP_INTERVAL_MS);
+      } else {
+        resolve();
+      }
+    }
+
+    moveCat();
   });
 }
 
@@ -21,6 +36,14 @@ function dance(img) {
     // and, after a timeout, reset the `img` back to the walking cat. Then
     // resolve the promise.
     // Make good use of the `DANCING_CAT_URL` and `DANCE_TIME_MS` constants.
+
+    const originalSrc = img.src;
+    img.src = DANCING_CAT_URL;
+
+    setTimeout(() => {
+      img.src = originalSrc;
+      resolve();
+    }, DANCE_TIME_MS);
   });
 }
 
@@ -35,6 +58,15 @@ function catWalk() {
   // 2. Then dance for 5 secs.
   // 3. Then walk from `centerPos` to `stopPos`.
   // 4. Repeat the first three steps indefinitely.
+
+  function startWalking() {
+    walk(img, startPos, centerPos)
+        .then(() => dance(img))
+        .then(() => walk(img, centerPos, stopPos))
+        .then(() => startWalking());
+  }
+
+  startWalking();
 }
 
 window.addEventListener('load', catWalk);
